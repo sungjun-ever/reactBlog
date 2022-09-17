d720305e147f7df9c4ddd868fe4b27264ebedfcd
unknown <zkfmak10@gmail.com>
2022-09-17 14:59:12 +0900

    pagination

--- resources/js/components/layouts/ShowIndex.jsx
+++ resources/js/components/layouts/ShowIndex.jsx
@@ -1,11 +1,14 @@
 import {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
+import Pagination from "./Pageination";
 
 
 export default function ShowIndex(){
     const [posts, setPosts] = useState([]);
     const [page, setPage] = useState(1);
     const [limit, setLimit] = useState(10);
+    const offset = (page - 1) * limit;
+    const [total, setTotal] = useState(0);
     useEffect(()=>{
         axios({
             method: 'GET',
@@ -18,23 +21,33 @@ export default function ShowIndex(){
         })
             .then(response => {
                 setPosts(response.data.post);
+                console.log(posts.length);
+                setTotal(posts.length);
             })
     }, []);
 
     return (
-        <div className={"h-screen flex"}>
-            <div className={"w-1/6"}>1</div>
-            <div className={"w-4/6"}>
-            {
-                posts.map((post)=>
-                    <div className={"flex border border-black justify-between px-4 h-16 items-center"} key={post.id}>
-                        <Link to={'/show/'+post.id} relative="path">Title : {post.title}</Link>
-                        <span>{post.userID}</span>
-                    </div>
-                )
-            }
+        <>
+            <div className={"h-screen flex"}>
+                <div className={"w-1/6"}>1</div>
+                <div className={"w-4/6"}>
+                {
+                    posts.slice(offset, offset + limit).map((post)=>
+                        <div className={"flex border border-black justify-between px-4 h-16 items-center"} key={post.id}>
+                            <Link to={'/show/'+post.id} relative="path">Title : {post.title}</Link>
+                            <span>{post.userID}</span>
+                        </div>
+                    )
+                }
+                </div>
+                <div className={"w-1/6"}>1</div>
             </div>
-            <div className={"w-1/6"}>1</div>
-        </div>
+            <Pagination
+                total={total}
+                limit={limit}
+                page={page}
+                setPage={setPage}
+            />
+        </>
     )
 }
