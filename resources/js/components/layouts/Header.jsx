c1a817bd03a76f3f4694a013c73db819d8aa49c9
unknown <zkfmak10@gmail.com>
2022-09-19 23:13:33 +0900

    header layout

--- resources/js/components/layouts/Header.jsx
+++ resources/js/components/layouts/Header.jsx
@@ -13,25 +13,27 @@ export default function Header(){
     }
 
     return (
-        <div className={"flex flex-auto border-b-2 border-b-zinc-500 justify-evenly h-16 items-center"}>
-            <div className={"w-1/4 text-center"}><Link to={"/"}>LOGO</Link></div>
-            <div className={"flex w-1/2 text-left"}>
-                <form action="" method={'GET'} className={"flex w-1/2 align-middle"}>
-                    <input type={'text'}
-                           name={'search'}
-                           className={"border-2 border-white rounded-md p-1 w-1/2 transition-all"}
-                           style={{backgroundColor:'#2d3436'}}
-                           placeholder={'Search..'}
-                           autoComplete={'off'}
-                           onFocus={longSearchBox}
-                           onBlur={shortSearchBox}
-                    />
-                    <button type={"submit"} className={"text-2xl pl-4"}><i className={"xi-search"}></i></button>
-                </form>
-            </div>
-            <div className="w-1/4 text-center">
-                <div>
-                    <button className={"text-3xl"}><i className={"xi-bars"}></i></button>
+        <div className={"border-b-2 border-b-zinc-500"}>
+            <div className={"flex flex-auto  justify-evenly h-16 items-center w-2/3 mx-auto"}>
+                <div className={"w-1/3"}><Link to={"/"}>LOGO</Link></div>
+                <div className={"flex w-1/3 text-left"}>
+                    <form action="" method={'GET'} className={"flex w-full align-middle"}>
+                        <input type={'text'}
+                               name={'search'}
+                               className={"border-2 border-white rounded-md p-1 w-1/2 transition-all"}
+                               style={{backgroundColor:'#2d3436'}}
+                               placeholder={'Search..'}
+                               autoComplete={'off'}
+                               onFocus={longSearchBox}
+                               onBlur={shortSearchBox}
+                        />
+                        <button type={"submit"} className={"text-2xl pl-4"}><i className={"xi-search"}></i></button>
+                    </form>
+                </div>
+                <div className="w-1/3 text-right">
+                    <div>
+                        <button className={"text-3xl"}><i className={"xi-bars"}></i></button>
+                    </div>
                 </div>
             </div>
         </div>
