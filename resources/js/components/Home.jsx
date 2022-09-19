da7d51ec6cdb3111a74c12843e3f17f94df417eb
unknown <zkfmak10@gmail.com>
2022-09-19 23:18:30 +0900

    home page

--- resources/js/components/Home.jsx
+++ resources/js/components/Home.jsx
@@ -1,46 +1,68 @@
 
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 export default function Home(){
+    const [posts, setPosts] = useState([]);
+
+    useEffect( ()=>{
+        axios({
+            method: 'GET',
+            url: '/api/show',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').attributes[1].value,
+            }
+        })
+            .then(response => {
+                setPosts(response.data.post);
+            })
+    }, [])
+
     return (
         <div className={"w-2/3 mx-auto py-16"}>
             <div className={"grid grid-cols-3 gap-x-4 gap-y-8 border"}>
-                <div className={"w-full h-48 border"}>
-                    <p className={"w-full p-2 h-36 border border-red-400"}>IMG</p>
-                    <p className={"w-full p-2 h-12 border border-blue-500"}>Title</p>
-                </div>
-                <div className={"w-full h-48 border"}>
-                    <p className={"w-full p-2 h-36 border border-red-400"}>IMG</p>
-                    <p className={"w-full p-2 h-12 border border-blue-500"}>Title</p>
-                </div>
-                <div className={"w-full h-48 border"}>
-                    <p className={"w-full p-2 h-36 border border-red-400"}>IMG</p>
-                    <p className={"w-full p-2 h-12 border border-blue-500"}>Title</p>
-                </div>
-                <div className={"w-full h-48 border"}>
-                    <p className={"w-full p-2 h-36 border border-red-400"}>IMG</p>
-                    <p className={"w-full p-2 h-12 border border-blue-500"}>Title</p>
-                </div>
-                <div className={"w-full h-48 border"}>
-                    <p className={"w-full p-2 h-36 border border-red-400"}>IMG</p>
-                    <p className={"w-full p-2 h-12 border border-blue-500"}>Title</p>
-                </div>
-                <div className={"w-full h-48 border"}>
-                    <p className={"w-full p-2 h-36 border border-red-400"}>IMG</p>
-                    <p className={"w-full p-2 h-12 border border-blue-500"}>Title</p>
-                </div>
-                <div className={"w-full h-48 border"}>
-                    <p className={"w-full p-2 h-36 border border-red-400"}>IMG</p>
-                    <p className={"w-full p-2 h-12 border border-blue-500"}>Title</p>
-                </div>
-                <div className={"w-full h-48 border"}>
-                    <p className={"w-full p-2 h-36 border border-red-400"}>IMG</p>
-                    <p className={"w-full p-2 h-12 border border-blue-500"}>Title</p>
-                </div>
-                <div className={"w-full h-48 border"}>
-                    <p className={"w-full p-2 h-36 border border-red-400"}>IMG</p>
-                    <p className={"w-full p-2 h-12 border border-blue-500"}>Title</p>
-                </div>
+                {
+                    posts.slice(0, 9).map((post)=>
+                        <div className={"w-full h-48 border"}>
+                            <p className={"w-full p-2 h-36 border border-red-400"}>IMG</p>
+                            <p className={"w-full p-2 h-12 border border-blue-500"}>{post.title}</p>
+                        </div>
+                    )
+                }
+
+                {/*<div className={"w-full h-48 border"}>*/}
+                {/*    <p className={"w-full p-2 h-36 border border-red-400"}>IMG</p>*/}
+                {/*    <p className={"w-full p-2 h-12 border border-blue-500"}>Title</p>*/}
+                {/*</div>*/}
+                {/*<div className={"w-full h-48 border"}>*/}
+                {/*    <p className={"w-full p-2 h-36 border border-red-400"}>IMG</p>*/}
+                {/*    <p className={"w-full p-2 h-12 border border-blue-500"}>Title</p>*/}
+                {/*</div>*/}
+                {/*<div className={"w-full h-48 border"}>*/}
+                {/*    <p className={"w-full p-2 h-36 border border-red-400"}>IMG</p>*/}
+                {/*    <p className={"w-full p-2 h-12 border border-blue-500"}>Title</p>*/}
+                {/*</div>*/}
+                {/*<div className={"w-full h-48 border"}>*/}
+                {/*    <p className={"w-full p-2 h-36 border border-red-400"}>IMG</p>*/}
+                {/*    <p className={"w-full p-2 h-12 border border-blue-500"}>Title</p>*/}
+                {/*</div>*/}
+                {/*<div className={"w-full h-48 border"}>*/}
+                {/*    <p className={"w-full p-2 h-36 border border-red-400"}>IMG</p>*/}
+                {/*    <p className={"w-full p-2 h-12 border border-blue-500"}>Title</p>*/}
+                {/*</div>*/}
+                {/*<div className={"w-full h-48 border"}>*/}
+                {/*    <p className={"w-full p-2 h-36 border border-red-400"}>IMG</p>*/}
+                {/*    <p className={"w-full p-2 h-12 border border-blue-500"}>Title</p>*/}
+                {/*</div>*/}
+                {/*<div className={"w-full h-48 border"}>*/}
+                {/*    <p className={"w-full p-2 h-36 border border-red-400"}>IMG</p>*/}
+                {/*    <p className={"w-full p-2 h-12 border border-blue-500"}>Title</p>*/}
+                {/*</div>*/}
+                {/*<div className={"w-full h-48 border"}>*/}
+                {/*    <p className={"w-full p-2 h-36 border border-red-400"}>IMG</p>*/}
+                {/*    <p className={"w-full p-2 h-12 border border-blue-500"}>Title</p>*/}
+                {/*</div>*/}
             </div>
         </div>
     )
