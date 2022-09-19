e8cae9dba4c09bb07aeff0d990822bea57969cd4
unknown <zkfmak10@gmail.com>
2022-09-19 23:09:22 +0900

    home page layout

--- resources/js/components/Home.jsx
+++ resources/js/components/Home.jsx
@@ -3,8 +3,45 @@ import React from "react";
 
 export default function Home(){
     return (
-        <div>
-            홈
+        <div className={"w-2/3 mx-auto py-16"}>
+            <div className={"grid grid-cols-3 gap-x-4 gap-y-8 border"}>
+                <div className={"w-full h-48 border"}>
+                    <p className={"w-full p-2 h-36 border border-red-400"}>IMG</p>
+                    <p className={"w-full p-2 h-12 border border-blue-500"}>Title</p>
+                </div>
+                <div className={"w-full h-48 border"}>
+                    <p className={"w-full p-2 h-36 border border-red-400"}>IMG</p>
+                    <p className={"w-full p-2 h-12 border border-blue-500"}>Title</p>
+                </div>
+                <div className={"w-full h-48 border"}>
+                    <p className={"w-full p-2 h-36 border border-red-400"}>IMG</p>
+                    <p className={"w-full p-2 h-12 border border-blue-500"}>Title</p>
+                </div>
+                <div className={"w-full h-48 border"}>
+                    <p className={"w-full p-2 h-36 border border-red-400"}>IMG</p>
+                    <p className={"w-full p-2 h-12 border border-blue-500"}>Title</p>
+                </div>
+                <div className={"w-full h-48 border"}>
+                    <p className={"w-full p-2 h-36 border border-red-400"}>IMG</p>
+                    <p className={"w-full p-2 h-12 border border-blue-500"}>Title</p>
+                </div>
+                <div className={"w-full h-48 border"}>
+                    <p className={"w-full p-2 h-36 border border-red-400"}>IMG</p>
+                    <p className={"w-full p-2 h-12 border border-blue-500"}>Title</p>
+                </div>
+                <div className={"w-full h-48 border"}>
+                    <p className={"w-full p-2 h-36 border border-red-400"}>IMG</p>
+                    <p className={"w-full p-2 h-12 border border-blue-500"}>Title</p>
+                </div>
+                <div className={"w-full h-48 border"}>
+                    <p className={"w-full p-2 h-36 border border-red-400"}>IMG</p>
+                    <p className={"w-full p-2 h-12 border border-blue-500"}>Title</p>
+                </div>
+                <div className={"w-full h-48 border"}>
+                    <p className={"w-full p-2 h-36 border border-red-400"}>IMG</p>
+                    <p className={"w-full p-2 h-12 border border-blue-500"}>Title</p>
+                </div>
+            </div>
         </div>
     )
 }
