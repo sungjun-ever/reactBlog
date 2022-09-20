e8e6e08af78bb0615b424459b54c1a30b3d50d64
unknown <zkfmak10@gmail.com>
2022-09-20 23:49:13 +0900

    showindex

--- resources/js/components/layouts/ShowIndex.jsx
+++ resources/js/components/layouts/ShowIndex.jsx
@@ -27,39 +27,36 @@ export default function ShowIndex(){
     }, [])
 
     return (
-        <>
-            <div className={"flex min-h-screen max-h-fit py-16"}>
-                <div className={"w-1/6"}>
-                    <SideMenu/>
-                </div>
-                <div className={"w-4/6 min-h"}>
-                    <div className={"flex pb-6 align-middle"}>
-                        <span className={"pr-2"}>per page : </span>
-                        <select name="page" className={"text-black rounded-md w-12"} onChange={({target: {value}}) => setLimit(value)}>
-                            <option value="10">10</option>
-                            <option value="15">15</option>
-                            <option value="20">20</option>
-                        </select>
-                    </div>
-                {
-                    posts.slice(offset, offset + limit).map((post)=>
-                        <div className={"flex border border-black justify-between px-4 h-16 items-center"} key={post.id}>
-                            <Link to={'/show/'+post.id} relative="path">Title : {post.title}</Link>
-                            <span>{post.userID}</span>
-                        </div>
-                    )
-                }
-                <Pagination
-                    total={total}
-                    limit={limit}
-                    page={page}
-                    setPage={setPage}
-                />
-                </div>
-                <div className={"w-1/6"}>1</div>
+        <div className={"flex min-h-screen max-h-fit py-16"}>
+            <div className={"w-1/6"}>
+                <SideMenu/>
             </div>
-
-        </>
+            <div className={"w-4/6 min-h"}>
+                <div className={"flex pb-6 align-middle"}>
+                    <span className={"pr-2"}>per page : </span>
+                    <select name="page" className={"text-black rounded-md w-12"} onChange={({target: {value}}) => setLimit(value)}>
+                        <option value="10">10</option>
+                        <option value="15">15</option>
+                        <option value="20">20</option>
+                    </select>
+                </div>
+            {
+                posts.slice(offset, offset + limit).map((post)=>
+                    <div className={"flex border border-black justify-between px-4 h-16 items-center"} key={post.id}>
+                        <Link to={'/show/'+post.id} relative="path">Title : {post.title}</Link>
+                        <span>{post.userID}</span>
+                    </div>
+                )
+            }
+            <Pagination
+                total={total}
+                limit={limit}
+                page={page}
+                setPage={setPage}
+            />
+            </div>
+            <div className={"w-1/6"}>1</div>
+        </div>
     )
 }
 
