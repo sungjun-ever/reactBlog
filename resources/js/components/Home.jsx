3e9c29925f36dc0af7489aee8e145a0429ced3bb
unknown <zkfmak10@gmail.com>
2022-09-20 20:25:45 +0900

    home layout

--- resources/js/components/Home.jsx
+++ resources/js/components/Home.jsx
@@ -22,18 +22,20 @@ export default function Home(){
 
     return (
         <div className={"w-2/3 mx-auto py-16"}>
-            <div className={"grid grid-cols-3 gap-x-4 gap-y-8 border"}>
+            <div className={"grid grid-cols-3 gap-x-4 gap-y-8"}>
                 {
                     posts.map((post)=>
                         <div className={"w-full h-60 border"} key={post.id}>
+                            <Link to={"/show/"+post.id}>
                             {
                                 post.url
-                                    ? <p className={"w-full h-48 border border-red-400"}>IMG</p>
-                                    : <p className={"w-full h-48 border border-red-400"}>
+                                    ? <p className={"w-full h-48"}>IMG</p>
+                                    : <p className={"w-full h-48"}>
                                         <img src={"../asset/noImage.jpg"} className={"w-full h-48"} loading={"lazy"}/>
                                     </p>
                             }
-                            <Link to={"/show/"+post.id}><p className={"w-full p-2 h-12 border border-blue-500"}>{post.title}</p></Link>
+                            <p className={"w-full py-2 px-1 h-12"}>{post.title}</p>
+                            </Link>
                         </div>
                     )
                 }
