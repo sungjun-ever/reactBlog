d4245775821902254573f096c135fa96baaa2037
unknown <zkfmak10@gmail.com>
2022-09-20 20:30:07 +0900

    header

--- resources/js/components/layouts/Header.jsx
+++ resources/js/components/layouts/Header.jsx
@@ -13,9 +13,15 @@ export default function Header(){
 
     return (
         <div className={"border-b-2 border-b-zinc-500"}>
-            <div className={"flex flex-auto  justify-evenly h-16 items-center w-2/3 mx-auto"}>
-                <div className={"w-1/3"}><Link to={"/"}>LOGO</Link></div>
-                <div className={"flex w-1/3"}>
+            <div className={"flex flex-auto justify-evenly h-16 items-center w-2/3 mx-auto"}>
+                <div className={"flex w-1/2 gap-x-12"}>
+                    <div>
+                        <button className={"text-3xl"}><i className={"xi-bars"}></i></button>
+                    </div>
+                    <Link to={"/"} className={"text-2xl"}>LOGO</Link>
+                </div>
+
+                <div className={"flex w-1/2"}>
                     <form action="" method={'GET'} className={"flex w-full align-middle justify-end"}>
                         <input type={'text'}
                                name={'search'}
@@ -28,11 +34,6 @@ export default function Header(){
                         />
                         <button type={"submit"} className={"text-2xl pl-4"}><i className={"xi-search"}></i></button>
                     </form>
-                </div>
-                <div className="w-1/3 text-right">
-                    <div>
-                        <button className={"text-3xl"}><i className={"xi-bars"}></i></button>
-                    </div>
                 </div>
             </div>
         </div>
