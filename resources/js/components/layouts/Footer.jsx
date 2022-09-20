f8db6a761fd49b94c9e34ebfd503e405d690bcc5
unknown <zkfmak10@gmail.com>
2022-09-20 20:41:01 +0900

    footer

--- resources/js/components/layouts/Footer.jsx
+++ resources/js/components/layouts/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer(){
     return (
-        <div className={"min-h-fit h-16 text-center py-4"}>
+        <div className={"min-h-fit h-16 text-center py-4 border-t-2 border-t-zinc-500"}>
             Copyright (c) All rights reserved.
         </div>
     )
